bfa2611d6b0335ccb0e8605dfa3059fa38548661
fwc <1643806656@qq.com>
2025-08-28 01:31:41 +0800

    fix: 修正QwenCredentials expiry字段名

--- pkg/qwen_code/main.go
+++ pkg/qwen_code/main.go
@@ -64,7 +64,7 @@ type DeviceTokenResponse struct {
 // QwenCredentials extends DeviceTokenResponse with an exact expiry time.
 type QwenCredentials struct {
 	DeviceTokenResponse
-	Expiry time.Time `json:"expiry_date"`
+	Expiry time.Time `json:"expiry"`
 }
 
 // generateCodeVerifier creates a random string for PKCE.
